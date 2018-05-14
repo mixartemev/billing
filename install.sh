738e38f2fa162c721cc3308188b2b94f93b9cd57
nastia <mixartemev@gmail.com>
2018-05-14 18:11:02 +0300

    install

--- install.sh
+++ install.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+
 #1. get dependencies
+info "Get dependencies"
 composer install
-composer install
-#4. set db access
+echo "Done!"
+
+#2. set db access
+info "Configure database access"
 sed -i "s/{db_login}/$1/" config/db.php
 sed -i "s/{db_password}/$2/" config/db.php
-#5. create db
+echo "Done!"
+
+#3. create db
+info "Create database"
 mysqladmin -u $1 -fp$2 create billing
-#6. fill db
+echo "Done!"
+
+#4. fill db
+info "Create database structure and fill it"
 php yii migrate --interactive=0
+echo "Done!"
