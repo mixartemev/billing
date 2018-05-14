1b479ecbc7c6057b60217204a1ba359a87eac8b7
nastia <mixartemev@gmail.com>
2018-05-14 17:59:31 +0300

    install

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 composer install
 composer install
 #4. set db access
-sed -i "s/{db_login}/$1/" ./config/db.php
-sed -i "s/{db_password}/$2/" ./config/db.php
+sed -i "s/{db_login}/$1/" config/db.php
+sed -i "s/{db_password}/$2/" config/db.php
 #5. create db
 mysqladmin -u $1 -fp$2 create billing
 #6. fill db
